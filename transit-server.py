33d405e9fa457724c72f08c8ac38813443e0daa5
michaelzhang114 <michaelzhang6284@gmail.com>
2023-05-26 00:38:31 +0100

    refactored startup logic

--- transit-server.py
+++ transit-server.py
@@ -7,21 +7,26 @@ FLASK_SERVER_PORT = 4999
 
 app = Flask(__name__)
 
+@app.before_first_request
+def setup():
+    get_gtfs_feed_static()
+    print("Retrieved GTFS static from King County Metro")
+    #print("Retrieved GTFS static from SoundTransit Open Transit Data")
+
 @app.route('/api', methods=['GET'])
 def my_api():
     # Retrieve the query parameters from the request
     route_id = request.args.get('route_id')
     stop_id = request.args.get('stop_id')
 
+    print("Looking for route_id={} and stop_id={}".format(route_id, stop_id))
+
+
     # Validate the query parameters
     if not route_id or not stop_id:
         return jsonify(error='Missing query parameters'), 400
 
-    print(route_id)
-    
-    # Process the query parameters
-    # TODO: Implement your logic here
-    get_gtfs_feed_static()
+
     output_json = get_gtfs_rt_my_stop_updates(route_id, stop_id)
     return jsonify(output_json)
 
