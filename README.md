3bda325c38550417f543e53e0900fc7fc9d56ba1
Michael Zhang <michaelzhang@mz-m1.lan>
2023-05-24 17:22:33 -0700

    mvp server done

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 ## Description
 
 ## Usage
+
+## Todo
+1. UI input: Bus route name and direction (visual), and pick the stop -> backend: route_id, direction_id, stop_id
+
+To make the time est logic better:
+- need to get direction_id from route_id + stop_id
+- from trip_id, get list of stop_id
+
+
+
+
+
+1. Get all trips from static gtfs trips.txt
+2. Given a trip ID, get the shape ID (static trips.txt)
+3. From the shape ID, get the coordinates of all the stops
+
+
---
+++ transit-server.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from gtfs import get_gtfs_feed_static, get_gtfs_rt_my_stop_updates
+import json
+
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def my_api():
+    # Retrieve the query parameters from the request
+    route_id = request.args.get('route_id')
+    stop_id = request.args.get('stop_id')
+
+    # Validate the query parameters
+    if not route_id or not stop_id:
+        return jsonify(error='Missing query parameters'), 400
+
+    print(route_id)
+    
+    # Process the query parameters
+    # TODO: Implement your logic here
+    get_gtfs_feed_static()
+    output_json = get_gtfs_rt_my_stop_updates(route_id, stop_id)
+    return jsonify(output_json)
+
+if __name__ == '__main__':
+    app.run('192.168.86.41', port=4999)
+
+#192.168.86.41:4999/api?route_id=100252&stop_id=6190
